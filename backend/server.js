f3246edecff482f72a099f16e9af7fde8fd9f868
AKhila <merugu.akhila21@gmail.com>
2026-04-29 15:29:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -65,10 +65,18 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
   }
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type,Authorization");
+  
+  const reqHeaders = req.headers["access-control-request-headers"];
+  if (reqHeaders) {
+    res.setHeader("Access-Control-Allow-Headers", reqHeaders);
+  } else {
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, bypass-tunnel-reminder");
+  }
+  
   res.setHeader("Access-Control-Allow-Credentials", "true");
+  
   if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
+    return res.status(200).end();
   }
   next();
 });
